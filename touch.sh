3b8111ab6d685747b81dfa12bf3e19098fbc1789
Henry Griffiths <42880130+henrygriffiths@users.noreply.github.com>
2022-05-22 22:50:13 -0500

    feat: init script

---
+++ touch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo Running Touch Script
+
+echo Mapping input 15
+xinput map-to-output 15 DP-1-1
+echo Mapping input 16
+xinput map-to-output 16 DP-1-2
+echo Mapping input 17
+xinput map-to-output 17 DP-1-1
+echo Mapping input 18
+xinput map-to-output 18 DP-1-1
+echo Mapping input 19
+xinput map-to-output 19 HDMI-1
+echo Mapping input 20
+xinput map-to-output 20 DP-1-1
+
+echo Done
